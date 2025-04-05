c83bc555f51377a303b84e590c4fbc92cf57a699
vince-smdt <vince.smdt@gmail.com>
2025-04-05 15:59:28 -0400

    Make valve buttons unclickable

--- src/windows/ControlsWindow.cpp
+++ src/windows/ControlsWindow.cpp
@@ -4,74 +4,9 @@
 
 namespace ControlsWindow {
 ValveState valveState1{ValveState::CLOSED};
-std::chrono::time_point<std::chrono::system_clock> valveTimer1{};
 ValveState valveState2{ValveState::CLOSED};
-std::chrono::time_point<std::chrono::system_clock> valveTimer2{};
 SerialCom serialTest{};
 } // namespace ControlsWindow
-
-bool ValveButton(const char* id, ValveState state) {
-    bool buttonPressed = false;
-    bool buttonDisabled = false;
-    ImVec4 buttonColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-    ImVec4 buttonHoveredColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-    ImVec4 buttonPressedColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-    const char* buttonLabel = "Undefined";
-
-    if (state == ValveState::OPEN) {
-        buttonColor = ImVec4(0.0f, 0.6f, 0.0f, 1.0f);
-        buttonHoveredColor = ImVec4(0.0f, 0.5f, 0.0f, 1.0f);
-        buttonPressedColor = ImVec4(0.0f, 0.4f, 0.0f, 1.0f);
-        buttonLabel = "Open";
-    } else if (state == ValveState::CLOSED) {
-        buttonColor = ImVec4(0.6f, 0.0f, 0.0f, 1.0f);
-        buttonHoveredColor = ImVec4(0.5f, 0.0f, 0.0f, 1.0f);
-        buttonPressedColor = ImVec4(0.4f, 0.0f, 0.0f, 1.0f);
-        buttonLabel = "Closed";
-    } else if (state == ValveState::OPENING) {
-        buttonColor = ImVec4(0.0f, 0.6f, 0.0f, 1.0f);
-        buttonLabel = "Opening";
-        buttonDisabled = true;
-    } else if (state == ValveState::CLOSING) {
-        buttonColor = ImVec4(0.6f, 0.0f, 0.0f, 1.0f);
-        buttonLabel = "Closing";
-        buttonDisabled = true;
-    }
-
-    ImGui::PushStyleColor(ImGuiCol_Button, buttonColor);
-    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, buttonHoveredColor);
-    ImGui::PushStyleColor(ImGuiCol_ButtonActive, buttonPressedColor);
-    ImGui::BeginDisabled(buttonDisabled);
-    std::string buttonID = std::string(buttonLabel) + "##" + id;
-    buttonPressed = ImGui::Button(buttonID.c_str(), ImVec2(200.0f, 0.0f));
-    ImGui::EndDisabled();
-    ImGui::PopStyleColor(3);
-    return buttonPressed;
-}
-
-void updateValveState(ValveState& state, std::chrono::time_point<std::chrono::system_clock>& timer, bool buttonPressed) {
-    auto now = std::chrono::system_clock::now();
-
-    if (state == ValveState::OPEN) {
-        if (buttonPressed) {
-            state = ValveState::CLOSING;
-            timer = now + std::chrono::seconds(1);
-        }
-    } else if (state == ValveState::CLOSED) {
-        if (buttonPressed) {
-            state = ValveState::OPENING;
-            timer = now + std::chrono::seconds(1);
-        }
-    } else if (state == ValveState::OPENING) {
-        if (now >= timer) {
-            state = ValveState::OPEN;
-        }
-    } else if (state == ValveState::CLOSING) {
-        if (now >= timer) {
-            state = ValveState::CLOSED;
-        }
-    }
-}
 
 namespace ControlsWindow {
 void init() {
@@ -82,13 +17,11 @@ void render() {
     if (ImGui::CollapsingHeader("Valves")) {
         ImGui::TextUnformatted("Valve 1");
         ImGui::SameLine();
-        bool valveButton1Pressed = ValveButton("valve1", valveState1);
-        updateValveState(valveState1, valveTimer1, valveButton1Pressed);
+        renderValveState("valve1", valveState1);
 
         ImGui::TextUnformatted("Valve 2");
         ImGui::SameLine();
-        bool valveButton2Pressed = ValveButton("valve2", valveState2);
-        updateValveState(valveState2, valveTimer2, valveButton2Pressed);
+        renderValveState("valve2", valveState2);
     }
 
     static char sendPacket[13] = "bonjour hi! ";
@@ -134,6 +67,32 @@ void render() {
     }
 
     serialTest.readChar();
+}
+
+void renderValveState(const char* id, ValveState state) {
+    ImVec4 buttonColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+    const char* buttonLabel = "Undefined";
+
+    if (state == ValveState::OPEN) {
+        buttonColor = ImVec4(0.0f, 0.6f, 0.0f, 1.0f);
+        buttonLabel = "Open";
+    } else if (state == ValveState::CLOSED) {
+        buttonColor = ImVec4(0.6f, 0.0f, 0.0f, 1.0f);
+        buttonLabel = "Closed";
+    } else if (state == ValveState::OPENING) {
+        buttonColor = ImVec4(0.0f, 0.6f, 0.0f, 1.0f);
+        buttonLabel = "Opening";
+    } else if (state == ValveState::CLOSING) {
+        buttonColor = ImVec4(0.6f, 0.0f, 0.0f, 1.0f);
+        buttonLabel = "Closing";
+    }
+
+    ImGui::PushStyleColor(ImGuiCol_Button, buttonColor);
+    ImGui::BeginDisabled();
+    std::string buttonID = std::string(buttonLabel) + "##" + id;
+    ImGui::Button(buttonID.c_str(), ImVec2(200.0f, 0.0f));
+    ImGui::EndDisabled();
+    ImGui::PopStyleColor(1);
 }
 
 void shutdown() {
--- src/windows/ControlsWindow.h
+++ src/windows/ControlsWindow.h
@@ -5,17 +5,16 @@
 
 #include <chrono>
 
+namespace ControlsWindow {
 enum class ValveState { OPEN, CLOSED, OPENING, CLOSING };
 
-namespace ControlsWindow {
 void init();
 void render();
+void renderValveState(const char* id, ValveState state);
 void shutdown();
 
 extern ValveState valveState1;
-extern std::chrono::time_point<std::chrono::system_clock> valveTimer1;
 extern ValveState valveState2;
-extern std::chrono::time_point<std::chrono::system_clock> valveTimer2;
 extern SerialCom serialCom;
 } // namespace ControlsWindow
 
