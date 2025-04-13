a09ac4b758e1a9813985bbc0a49cf35b4b03109a
vince-smdt <vince.smdt@gmail.com>
2025-04-13 17:12:40 -0400

    Fix RecvBuffer tests

--- tests/TestRecvBuffer.cpp
+++ tests/TestRecvBuffer.cpp
@@ -48,16 +48,16 @@ TEST_CASE("Should detect correct amount of packets during writes") {
     RecvBuffer<TEST_RECVBUFFER_SIZE> recvBuf;
 
     SUBCASE("Correct header codes") {
-        CHECK(writeToRecvBuffer(recvBuf, "ACC\0a1234acc test", 17));
+        CHECK(writeToRecvBuffer(recvBuf, "\0CCAa1234acc test", 17));
         CHECK(recvBuf.availablePackets() == 0);
 
-        CHECK(writeToRecvBuffer(recvBuf, "asdfGYR\x002qwerty", 14));
+        CHECK(writeToRecvBuffer(recvBuf, "asdf\x002RYGqwerty", 14));
         CHECK(recvBuf.availablePackets() == 1);
 
-        CHECK(writeToRecvBuffer(recvBuf, "zxcvbnmVLV\x010", 11));
+        CHECK(writeToRecvBuffer(recvBuf, "zxcvbnm\x010VLV", 11));
         CHECK(recvBuf.availablePackets() == 2);
 
-        CHECK(writeToRecvBuffer(recvBuf, "ALT\0GPS\x044MAG\0abcdefgPRS\0", 23));
+        CHECK(writeToRecvBuffer(recvBuf, "\0TLA\x044SPG\0GAMabcdefg\0SRP", 23));
         CHECK(recvBuf.availablePackets() == 6);
     }
 
@@ -76,19 +76,19 @@ TEST_CASE("Should detect correct amount of packets during writes") {
     }
 
     SUBCASE("Header codes split through multiple writes") {
-        CHECK(writeToRecvBuffer(recvBuf, "1234 A", 6));
-        CHECK(writeToRecvBuffer(recvBuf, "CC\0abcd", 7));
+        CHECK(writeToRecvBuffer(recvBuf, "1234 \0C", 7));
+        CHECK(writeToRecvBuffer(recvBuf, "CAabcd", 6));
         CHECK(recvBuf.availablePackets() == 0);
 
-        CHECK(writeToRecvBuffer(recvBuf, "5678 TH", 7));
-        CHECK(writeToRecvBuffer(recvBuf, "M\0efgh", 6));
+        CHECK(writeToRecvBuffer(recvBuf, "5678 \0M", 7));
+        CHECK(writeToRecvBuffer(recvBuf, "HTefgh", 6));
         CHECK(recvBuf.availablePackets() == 1);
 
-        CHECK(writeToRecvBuffer(recvBuf, "9012 VLV", 8));
-        CHECK(writeToRecvBuffer(recvBuf, "\x001ijkl", 5));
+        CHECK(writeToRecvBuffer(recvBuf, "9012 \x001", 8));
+        CHECK(writeToRecvBuffer(recvBuf, "VLVijkl", 7));
         CHECK(recvBuf.availablePackets() == 2);
 
-        CHECK(writeToRecvBuffer(recvBuf, "PRS\0", 4));
+        CHECK(writeToRecvBuffer(recvBuf, "\0SRP", 4));
         CHECK(recvBuf.availablePackets() == 3);
     }
 
@@ -97,29 +97,29 @@ TEST_CASE("Should detect correct amount of packets during writes") {
         size_t dataSize;
 
         SUBCASE("Split after 1st header code character") {
-            CHECK(writeToRecvBuffer(recvBuf, "ACC\0ACC\0", 8));
+            CHECK(writeToRecvBuffer(recvBuf, "\0CCA\0CCA", 8));
             CHECK(fillRecvBuffer(recvBuf, 91));
             dataSize = recvBuf.readPacket(rcv);
             CHECK(dataSize == 4);
-            CHECK(writeToRecvBuffer(recvBuf, "ACC\0", 4));
+            CHECK(writeToRecvBuffer(recvBuf, "\0CCA", 4));
             CHECK(recvBuf.availablePackets() == 1);
         }
 
         SUBCASE("Split after 2st header code character") {
-            CHECK(writeToRecvBuffer(recvBuf, "ACC\0ACC\0", 8));
+            CHECK(writeToRecvBuffer(recvBuf, "\0CCA\0CCA", 8));
             CHECK(fillRecvBuffer(recvBuf, 90));
             dataSize = recvBuf.readPacket(rcv);
             CHECK(dataSize == 4);
-            CHECK(writeToRecvBuffer(recvBuf, "ACC\0", 4));
+            CHECK(writeToRecvBuffer(recvBuf, "\0CCA", 4));
             CHECK(recvBuf.availablePackets() == 1);
         }
 
         SUBCASE("Split after 3st header code character") {
-            CHECK(writeToRecvBuffer(recvBuf, "ACC\0ACC\0", 8));
+            CHECK(writeToRecvBuffer(recvBuf, "\0CCA\0CCA", 8));
             CHECK(fillRecvBuffer(recvBuf, 89));
             dataSize = recvBuf.readPacket(rcv);
             CHECK(dataSize == 4);
-            CHECK(writeToRecvBuffer(recvBuf, "ACC\0", 4));
+            CHECK(writeToRecvBuffer(recvBuf, "\0CCA", 4));
             CHECK(recvBuf.availablePackets() == 1);
         }
     }
@@ -131,30 +131,30 @@ TEST_CASE("Should read packets correctly") {
     size_t dataSize;
 
     SUBCASE("Packet with no data") {
-        CHECK(writeToRecvBuffer(recvBuf, "ACC\0ACC\x001", 8));
+        CHECK(writeToRecvBuffer(recvBuf, "\0CCA\0CCA", 8));
         dataSize = recvBuf.readPacket(rcv);
         CHECK(dataSize == 4);
     }
 
     SUBCASE("Packet with data") {
-        CHECK(writeToRecvBuffer(recvBuf, "PRS\0abcd1234RKT\0", 16));
+        CHECK(writeToRecvBuffer(recvBuf, "\0SRPabcd1234\0TKR", 16));
         dataSize = recvBuf.readPacket(rcv);
         CHECK(dataSize == 12);
-        CHECK(msgEqual(rcv, "PRS\0abcd1234", dataSize));
+        CHECK(msgEqual(rcv, "\0SRPabcd1234", dataSize));
     }
 
     SUBCASE("Packet with data split between beginning and end of circular buffer") {
-        CHECK(writeToRecvBuffer(recvBuf, "ACC\0", 4));
+        CHECK(writeToRecvBuffer(recvBuf, "\0CCA", 4));
         CHECK(fillRecvBuffer(recvBuf, 84));
-        CHECK(writeToRecvBuffer(recvBuf, "ACC\0", 4));
+        CHECK(writeToRecvBuffer(recvBuf, "\0CCA", 4));
         dataSize = recvBuf.readPacket(rcv); // Clear packet (0-87)
         CHECK(dataSize == 88);
-        CHECK(writeToRecvBuffer(recvBuf, "ALT\0efgh5678GPS\0", 16));
+        CHECK(writeToRecvBuffer(recvBuf, "\0TLAefgh5678\0SPG", 16));
         dataSize = recvBuf.readPacket(rcv); // Clear packet (88-91)
         CHECK(dataSize == 4);
         dataSize = recvBuf.readPacket(rcv); // Read target packet (92-7)
         CHECK(dataSize == 12);
-        CHECK(msgEqual(rcv, "ALT\0efgh5678", dataSize));
+        CHECK(msgEqual(rcv, "\0TLAefgh5678", dataSize));
     }
 }
 
@@ -164,10 +164,10 @@ TEST_CASE("Should detect when the buffer is full") {
     size_t dataSize;
 
     CHECK_FALSE(recvBuf.isFull());
-    CHECK(writeToRecvBuffer(recvBuf, "ACC\0", 4));
+    CHECK(writeToRecvBuffer(recvBuf, "\0CCA", 4));
     CHECK(fillRecvBuffer(recvBuf, 46));
     CHECK_FALSE(recvBuf.isFull());
-    CHECK(writeToRecvBuffer(recvBuf, "ACC\0", 4));
+    CHECK(writeToRecvBuffer(recvBuf, "\0CCA", 4));
     CHECK(fillRecvBuffer(recvBuf, 46));
     CHECK(recvBuf.isFull());
     dataSize = recvBuf.readPacket(rcv);
@@ -180,7 +180,7 @@ TEST_CASE("Should detect when the buffer is full") {
 TEST_CASE("Should clear correctly") {
     RecvBuffer<TEST_RECVBUFFER_SIZE> recvBuf;
 
-    CHECK(writeToRecvBuffer(recvBuf, "ACC\0abcdVLV\x00AzaaaaaaaGPS\0qwertyazertyTHM\0", 40));
+    CHECK(writeToRecvBuffer(recvBuf, "\0CCAabcd\x00AVLVzaaaaaaa\0SPGqwertyazerty\0MHT", 40));
     recvBuf.clear();
     CHECK(recvBuf.availablePackets() == 0);
     CHECK_FALSE(recvBuf.isFull());
