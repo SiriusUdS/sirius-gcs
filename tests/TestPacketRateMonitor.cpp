8a1b86c736907a9eeecf9b1277c951100d347114
vince-smdt <vince.smdt@gmail.com>
2025-06-09 23:08:39 -0400

    Fix TestPacketRateMonitor

--- tests/TestPacketRateMonitor.cpp
+++ tests/TestPacketRateMonitor.cpp
@@ -2,14 +2,24 @@
 
 #include <doctest.h>
 
-constexpr double PRM_EPSILON = 0.01;
+constexpr double PRM_EPSILON = 1;
 
 void simulatePacketCom(PacketRateMonitor& prm, size_t numPackets, double durationSec) {
     const double TIME_BETWEEN_PACKETS_SEC = durationSec / numPackets;
+    auto startTime = std::chrono::steady_clock::now();
 
     for (size_t packetIdx = 0; packetIdx < numPackets; packetIdx++) {
-        std::this_thread::sleep_for(std::chrono::duration<double>(TIME_BETWEEN_PACKETS_SEC));
+        auto idealNextTime = startTime + std::chrono::duration<double>(TIME_BETWEEN_PACKETS_SEC * (packetIdx + 1));
+
         prm.trackPacket();
+
+        auto now = std::chrono::steady_clock::now();
+        auto sleepDuration = idealNextTime - now;
+
+        while (sleepDuration > std::chrono::duration<double>::zero()) {
+            now = std::chrono::steady_clock::now();
+            sleepDuration = idealNextTime - now;
+        }
     }
 }
 
@@ -18,6 +28,14 @@ TEST_CASE("Get packet rate with packet rate monitor") {
 
     simulatePacketCom(prm, 15, 0.5);
     CHECK(doctest::Approx(prm.getRatePerSecond()).epsilon(PRM_EPSILON) == 30);
-    simulatePacketCom(prm, 1000, 0.2);
-    CHECK(doctest::Approx(prm.getRatePerSecond()).epsilon(PRM_EPSILON) == 10000);
+    simulatePacketCom(prm, 100, 0.2);
+    CHECK(doctest::Approx(prm.getRatePerSecond()).epsilon(PRM_EPSILON) == 500);
+
+    prm.reset();
+    simulatePacketCom(prm, 2, 0.1);
+    CHECK(doctest::Approx(prm.getRatePerSecond()).epsilon(PRM_EPSILON) == 20);
+
+    prm.reset();
+    simulatePacketCom(prm, 5, 0.4);
+    CHECK(doctest::Approx(prm.getRatePerSecond()).epsilon(PRM_EPSILON) == 12);
 }
