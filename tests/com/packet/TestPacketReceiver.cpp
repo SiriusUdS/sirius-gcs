1f99f09d7ce6856682b0617cfa00f74ab1327433
vince-smdt <vince.smdt@gmail.com>
2025-08-05 21:27:40 -0400

    Improve PacketReceiver tests

--- tests/com/packet/TestPacketReceiver.cpp
+++ tests/com/packet/TestPacketReceiver.cpp
@@ -5,7 +5,7 @@
 
 #include <doctest.h>
 
-void writeTelemetryPacket(PacketReceiver& pr) {
+void writeEngineTelemetryPacket(PacketReceiver& pr) {
     EngineTelemetryPacket packet;
     packet.fields.header.bits.type = TELEMETRY_TYPE_CODE;
     packet.fields.header.bits.boardId = ENGINE_BOARD_ID;
@@ -19,6 +19,52 @@ void writeTelemetryPacket(PacketReceiver& pr) {
     }
 }
 
+void writeFillingStationTelemetryPacket(PacketReceiver& pr) {
+    FillingStationTelemetryPacket packet;
+    packet.fields.header.bits.type = TELEMETRY_TYPE_CODE;
+    packet.fields.header.bits.boardId = FILLING_STATION_BOARD_ID;
+    for (uint16_t i = 0; i < ENGINE_ADC_CHANNEL_AMOUNT; i++) {
+        packet.fields.adcValues[i] = 0x5678 + i;
+    }
+    packet.fields.crc = 0x87654321;
+
+    for (size_t i = 0; i < sizeof(FillingStationTelemetryPacket); i++) {
+        pr.receiveByte(packet.data[i]);
+    }
+}
+
+void checkEngineTelemetryPacket(PacketReceiver& pr) {
+    EngineTelemetryPacket packet;
+
+    CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
+    CHECK(pr.nextPacketMetadata()->size == sizeof(EngineTelemetryPacket));
+
+    pr.getPacket(packet.data);
+
+    CHECK(packet.fields.header.bits.type == TELEMETRY_TYPE_CODE);
+    CHECK(packet.fields.header.bits.boardId == ENGINE_BOARD_ID);
+    for (uint16_t i = 0; i < ENGINE_ADC_CHANNEL_AMOUNT; i++) {
+        CHECK(packet.fields.adcValues[i] == 0x1234 + i);
+    }
+    CHECK(packet.fields.crc == 0x12345678);
+}
+
+void checkFillingStationTelemetryPacket(PacketReceiver& pr) {
+    FillingStationTelemetryPacket packet;
+
+    CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
+    CHECK(pr.nextPacketMetadata()->size == sizeof(FillingStationTelemetryPacket));
+
+    pr.getPacket(packet.data);
+
+    CHECK(packet.fields.header.bits.type == TELEMETRY_TYPE_CODE);
+    CHECK(packet.fields.header.bits.boardId == FILLING_STATION_BOARD_ID);
+    for (uint16_t i = 0; i < ENGINE_ADC_CHANNEL_AMOUNT; i++) {
+        CHECK(packet.fields.adcValues[i] == 0x5678 + i);
+    }
+    CHECK(packet.fields.crc == 0x87654321);
+}
+
 void fill(PacketReceiver& pr, size_t size) {
     for (size_t i = 0; i < size; i++) {
         pr.receiveByte(0);
@@ -29,41 +75,39 @@ TEST_CASE("PacketReceiver should receive packet") {
     PacketReceiver pr;
 
     CHECK_FALSE(pr.nextPacketMetadata().has_value());
-    writeTelemetryPacket(pr);
+    writeEngineTelemetryPacket(pr);
     CHECK_FALSE(pr.nextPacketMetadata().has_value());
-    writeTelemetryPacket(pr);
-    CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
-    CHECK(pr.nextPacketMetadata()->size == sizeof(EngineTelemetryPacket));
+    writeEngineTelemetryPacket(pr);
+    CHECK(pr.nextPacketMetadata().has_value());
 }
 
 TEST_CASE("PacketReceiver should be able to get packet") {
     PacketReceiver pr;
-    EngineTelemetryPacket packet;
 
-    writeTelemetryPacket(pr);
-    writeTelemetryPacket(pr);
-    CHECK(pr.getPacket(packet.data));
-    CHECK(packet.fields.adcValues[0] == 0x1234);
+    writeEngineTelemetryPacket(pr);
+    writeEngineTelemetryPacket(pr);
+    checkEngineTelemetryPacket(pr);
 }
 
 TEST_CASE("PacketReceiver should dump next packet") {
     PacketReceiver pr;
+    uint8_t buf[sizeof(EngineTelemetryPacket) + sizeof(FillingStationTelemetryPacket)] = {0};
 
-    writeTelemetryPacket(pr);
-    writeTelemetryPacket(pr);
+    writeEngineTelemetryPacket(pr);
+    writeEngineTelemetryPacket(pr);
     fill(pr, 4);
-    writeTelemetryPacket(pr);
+    writeFillingStationTelemetryPacket(pr);
     fill(pr, 8);
     CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
     CHECK(pr.nextPacketMetadata()->size == sizeof(EngineTelemetryPacket));
     CHECK(pr.dumpNextPacket());
     CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
     CHECK(pr.nextPacketMetadata()->size == sizeof(EngineTelemetryPacket) + 4);
-    writeTelemetryPacket(pr);
+    writeEngineTelemetryPacket(pr);
     fill(pr, 1000);
     CHECK(pr.dumpNextPacket());
     CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
-    CHECK(pr.nextPacketMetadata()->size == sizeof(EngineTelemetryPacket) + 8);
+    CHECK(pr.nextPacketMetadata()->size == sizeof(FillingStationTelemetryPacket) + 8);
     CHECK(pr.dumpNextPacket());
     CHECK_FALSE(pr.nextPacketMetadata().has_value());
     CHECK_FALSE(pr.dumpNextPacket());
@@ -74,25 +118,34 @@ TEST_CASE("PacketReceiver internal buffer reset") {
 
     SUBCASE("PacketReceiver should reset when the internal buffer is full and no packets are available") {
         fill(pr, SerialConfig::PACKET_CIRCULAR_BUFFER_SIZE);
-        writeTelemetryPacket(pr);
-        writeTelemetryPacket(pr);
-        CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
-        CHECK(pr.nextPacketMetadata()->size == sizeof(EngineTelemetryPacket));
+        writeFillingStationTelemetryPacket(pr);
+        writeEngineTelemetryPacket(pr);
+        checkFillingStationTelemetryPacket(pr);
     }
 
     SUBCASE("PacketReceiver should not reset when the interval buffer is full and packets are available") {
         uint8_t recv[sizeof(EngineTelemetryPacket)] = {0};
 
-        writeTelemetryPacket(pr);
-        writeTelemetryPacket(pr);
-        CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
-        fill(pr, SerialConfig::PACKET_CIRCULAR_BUFFER_SIZE - sizeof(EngineTelemetryPacket));
-        writeTelemetryPacket(pr);
-        writeTelemetryPacket(pr);
-        writeTelemetryPacket(pr);
+        writeEngineTelemetryPacket(pr);
+        writeEngineTelemetryPacket(pr);
         CHECK(pr.nextPacketMetadata()->status == PacketMetadata::Status::VALID);
         CHECK(pr.nextPacketMetadata()->size == sizeof(EngineTelemetryPacket));
-        pr.getPacket(recv);
+        fill(pr, SerialConfig::PACKET_CIRCULAR_BUFFER_SIZE - sizeof(EngineTelemetryPacket));
+        writeFillingStationTelemetryPacket(pr);
+        writeEngineTelemetryPacket(pr);
+        writeFillingStationTelemetryPacket(pr);
+        checkEngineTelemetryPacket(pr);
         CHECK_FALSE(pr.nextPacketMetadata().has_value());
     }
+}
+
+TEST_CASE("PacketReceiver should correctly receive packets even if initial data was junk") {
+    PacketReceiver pr;
+
+    fill(pr, 13);
+    writeFillingStationTelemetryPacket(pr);
+    writeEngineTelemetryPacket(pr);
+    checkFillingStationTelemetryPacket(pr);
+    writeFillingStationTelemetryPacket(pr);
+    checkEngineTelemetryPacket(pr);
 }
