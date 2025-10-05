1cba81186aca1541ff5d1d329614c9775b976ff5
vince-smdt <vince.smdt@gmail.com>
2025-10-05 13:14:41 -0400

    Fix small bug in RecentPlotData

--- src/plot/RecentPlotData.cpp
+++ src/plot/RecentPlotData.cpp
@@ -11,14 +11,15 @@ void RecentPlotData::plot(bool showCompressedData) {
     PlotData::LockedView view = plotData.makeLockedView();
     const PlotRawData& data = showCompressedData ? view.getCompressedData() : view.getData();
     const PlotStyle& style = view.getStyle();
+    const size_t size = data.size();
 
-    if (data.size() == 0) {
+    if (size == 0) {
         return;
     }
 
     const float minX = data.lastX() - windowX;
 
-    if (start >= data.size()) {
+    if (start >= size) {
         start = 0;
     }
 
@@ -26,11 +27,11 @@ void RecentPlotData::plot(bool showCompressedData) {
         start--;
     }
 
-    while (start + 1 < data.size() && data.getXAt(start) < minX) {
+    while (start < size && data.getXAt(start) < minX) {
         start++;
     }
 
-    const size_t visibleCount = data.size() - start;
+    const size_t visibleCount = size - start;
     if (visibleCount) {
         ImPlot::SetNextLineStyle(style.color, style.weight);
         ImPlot::PlotLine(style.name, data.getRawX() + start, data.getRawY() + start, static_cast<int>(visibleCount));
